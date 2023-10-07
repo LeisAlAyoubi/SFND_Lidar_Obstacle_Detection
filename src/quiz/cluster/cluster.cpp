e0724a9e3c2e8f3c730a3751b8f543a046190ad5
Leis Al Ayoubi <leisayoubi@web.de>
2023-10-07 12:27:29 +0200

    -implemented kdtree - implemented euclidean clustering

--- src/quiz/cluster/cluster.cpp
+++ src/quiz/cluster/cluster.cpp
@@ -75,15 +75,56 @@ void render2DTree(Node* node, pcl::visualization::PCLVisualizer::Ptr& viewer, Bo
 
 }
 
+/**
+ * Recursive function to find nearby points within a specified Euclidean distance.
+ *
+ * @param points The list of points to search.
+ * @param tree A pointer to the KD-Tree data structure.
+ * @param pointIdx The index of the current point to start the search from.
+ * @param cluster A vector to store the indices of points in the cluster.
+ * @param distanceTol The maximum Euclidean distance to consider for proximity.
+ * @param processed A vector to keep track of processed points.
+ */
+void Proximity(const std::vector<std::vector<float>>& points, KdTree* tree, int pointIdx, std::vector<int>& cluster, float distanceTol, std::vector<bool>& processed)
+{
+    processed[pointIdx] = true;
+    cluster.push_back(pointIdx);
+
+    std::vector<int> nearbyPoints = tree->search(points[pointIdx], distanceTol);
+
+    for (int i : nearbyPoints)
+    {
+        if (!processed[i])
+        {
+            Proximity(points, tree, i, cluster, distanceTol, processed);
+        }
+    }
+}
+
+/**
+ * Cluster points in the input list based on Euclidean proximity using a KD-Tree.
+ *
+ * @param points The list of points to cluster.
+ * @param tree A pointer to the KD-Tree data structure.
+ * @param distanceTol The maximum Euclidean distance to consider for clustering.
+ * @return A vector of vectors, where each inner vector represents a cluster of points.
+ */
 std::vector<std::vector<int>> euclideanCluster(const std::vector<std::vector<float>>& points, KdTree* tree, float distanceTol)
 {
+    std::vector<std::vector<int>> clusters;
+    std::vector<bool> processed(points.size(), false);
 
-	// TODO: Fill out this function to return list of indices for each cluster
+    for (int i = 0; i < points.size(); ++i)
+    {
+        if (!processed[i])
+        {
+            std::vector<int> cluster;
+            Proximity(points, tree, i, cluster, distanceTol, processed);
+            clusters.push_back(cluster);
+        }
+    }
 
-	std::vector<std::vector<int>> clusters;
- 
-	return clusters;
-
+    return clusters;
 }
 
 int main ()
--- src/quiz/cluster/kdtree.h
+++ src/quiz/cluster/kdtree.h
@@ -39,18 +39,81 @@ struct KdTree
 	void insert(std::vector<float> point, int id)
 	{
 		// TODO: Fill in this function to insert a new point into the tree
-		// the function should create a new node and place correctly with in the root 
+		// the function should create a new node and place correctly with in the root
+		insertHelper(root, point, id, 0);
+	}
 
+	void insertHelper(Node *&node, std::vector<float> point, int id, int depth)
+	{
+		if (node == NULL)
+		{
+			node = new Node(point, id);
+		}
+		else
+		{
+			int cd = depth % point.size(); // Calculate current dimension to split on
+			if (point[cd] < node->point[cd])
+			{
+				insertHelper(node->left, point, id, depth + 1);
+			}
+			else
+			{
+				insertHelper(node->right, point, id, depth + 1);
+			}
+		}
 	}
 
 	// return a list of point ids in the tree that are within distance of target
 	std::vector<int> search(std::vector<float> target, float distanceTol)
 	{
 		std::vector<int> ids;
+		searchHelper(root, target, distanceTol, 0, ids);
 		return ids;
 	}
-	
 
+	void searchHelper(Node *node, std::vector<float> target, float distanceTol, int depth, std::vector<int> &ids)
+	{
+		if (node != NULL)
+		{
+			// Check if the current node's point is within the range
+			bool withinRange = true;
+			for (int i = 0; i < target.size(); ++i)
+			{
+				if (fabs(target[i] - node->point[i]) > distanceTol)
+				{
+					withinRange = false;
+					break;
+				}
+			}
+
+			if (withinRange)
+			{
+				// Calculate Euclidean distance between the target and the current point
+				float distance = 0;
+				for (int i = 0; i < target.size(); ++i)
+				{
+					distance += (target[i] - node->point[i]) * (target[i] - node->point[i]);
+				}
+				distance = sqrt(distance);
+
+				if (distance <= distanceTol)
+				{
+					ids.push_back(node->id);
+				}
+			}
+
+			// Recursively search the left and right subtrees
+			int cd = depth % target.size();
+			if (target[cd] - distanceTol < node->point[cd])
+			{
+				searchHelper(node->left, target, distanceTol, depth + 1, ids);
+			}
+			if (target[cd] + distanceTol > node->point[cd])
+			{
+				searchHelper(node->right, target, distanceTol, depth + 1, ids);
+			}
+		}
+	}
 };
 
 
